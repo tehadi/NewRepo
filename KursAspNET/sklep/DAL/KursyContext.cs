98ccdbddaf5fc40680e5779fcf6d8920263abe03
adrian <adrian@ADIKOMP>
2016-12-29 04:46:39 +0100

    dodanie context

---
+++ KursAspNET/sklep/DAL/KursyContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using sklep.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace sklep.DAL
+{
+    class KursyContext : DbContext
+    {
+        public KursyContext():base("KursyContext")
+        { }
+        public DbSet<Kurs> Kursy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
